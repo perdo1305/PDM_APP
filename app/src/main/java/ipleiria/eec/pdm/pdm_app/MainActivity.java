35996a8cfd180b247bdfb1b75b04608737eb3426
perdo1305 <pedroandre.sferreira@gmail.com>
2025-01-01 18:35:35 +0000

    semi working bluetooth

--- app/src/main/java/ipleiria/eec/pdm/pdm_app/MainActivity.java
+++ app/src/main/java/ipleiria/eec/pdm/pdm_app/MainActivity.java
@@ -7,7 +7,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
