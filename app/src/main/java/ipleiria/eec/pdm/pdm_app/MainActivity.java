08424a37f26ae29ebd16bef37ebfdb81f614f41c
perdo1305 <pedroandre.sferreira@gmail.com>
2025-01-05 18:32:29 +0000

    fix merge bugs

--- app/src/main/java/ipleiria/eec/pdm/pdm_app/MainActivity.java
+++ app/src/main/java/ipleiria/eec/pdm/pdm_app/MainActivity.java
@@ -74,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.action_settings) {
-            mostrarDialogoConfiguracoes();
+            showSettingsDialog();
             return true;
         } else if (id == R.id.action_help) {
             mostrarDialogoAjuda();
